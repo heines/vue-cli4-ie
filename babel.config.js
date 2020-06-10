de0858cc7d07bca012f2fbe982fc5db7fd5d74e3
清水園子 <s-shimizu@tsumikiinc.com>
2020-06-10 10:46:29 +0900

    eslint+prettier追加。npm run lint実行

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  presets: [
-    '@vue/app',
-  ],
+  presets: ["@vue/app"],
   plugins: [
-    'transform-custom-element-classes',
-    'transform-es2015-shorthand-properties',
-    'transform-es2015-template-literals',
+    "transform-custom-element-classes",
+    "transform-es2015-shorthand-properties",
+    "transform-es2015-template-literals",
     "@babel/plugin-transform-arrow-functions",
     "@babel/plugin-transform-regenerator"
   ]
-}
+};
